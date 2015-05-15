3e895fee139ce018b9922b9f80a376e80a5f36a9
Pontus Ullgren <pontus.ullgren@redpill-linpro.com>
2015-05-15 20:24:44 +0200

    Added test case for binary payload and upgraded to Mule 3.6.1

--- pgpexamples/src/test/java/com/ullgren/pontus/demo/SimpleIntegtationTest.java
+++ pgpexamples/src/test/java/com/ullgren/pontus/demo/SimpleIntegtationTest.java
@@ -3,7 +3,7 @@ package com.ullgren.pontus.demo;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.axiom.attachments.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mule.api.MuleMessage;
@@ -22,7 +22,6 @@ public class SimpleIntegtationTest extends  FunctionalTestCase {
 	{
 	    MuleClient client = new MuleClient(muleContext);
 	    String payload = "Hello World!";
-//	    String expectedPayload = IOUtils.toString(ClassLoader.getSystemResourceAsStream("encryptedmessage.asc"));
 
 	    Map<String, Object> properties = new HashMap<String, Object>();
 	    properties.put("MULE_USER", "Test Testsson (This is just a test key please do not trust it.) <test.testsson@mailinator.com>");
@@ -30,15 +29,15 @@ public class SimpleIntegtationTest extends  FunctionalTestCase {
 	    
 	    Assert.assertTrue("Payload does not start with expected PGP header",  result.getPayloadAsString().startsWith("-----BEGIN PGP MESSAGE-----"));
 	    // Uncomment to write the encrypted message to file so that we can validate it using "gpg -d < output.txt"
-//	    IOUtils.write(result.getPayloadAsString(), new FileOutputStream("output.txt"));
+	    // IOUtils.write(result.getPayloadAsString(), new FileOutputStream("output.txt"));
 	}
 	
-//	@Test
-	public void testDecryptPayload() throws Exception
+	@Test
+	public void testDecryptArmoredPayload() throws Exception
 	{
 	    MuleClient client = new MuleClient(muleContext);
-	    String expectedPayload = "Hello World!";
-	    String payload = new String(IOUtils.getStreamAsByteArray(ClassLoader.getSystemResourceAsStream("encryptedmessage.asc")), "UTF-8");
+	    String expectedPayload = "Hello World!\n";
+	    byte[] payload = IOUtils.toByteArray(ClassLoader.getSystemResourceAsStream("encryptedmessage.asc"));
 
 	    Map<String, Object> properties = new HashMap<String, Object>();
 	    properties.put("MULE_USER", "Test Testsson (This is just a test key please do not trust it.) <test.testsson@mailinator.com>");
@@ -46,7 +45,20 @@ public class SimpleIntegtationTest extends  FunctionalTestCase {
 	    Assert.assertEquals(expectedPayload, result.getPayloadAsString());
 	}
 
-//	@Test
+	@Test
+	public void testDecryptBinaryPayload() throws Exception
+	{
+	    MuleClient client = new MuleClient(muleContext);
+	    String expectedPayload = "Hello World!\n";
+	    byte[] payload = IOUtils.toByteArray(ClassLoader.getSystemResourceAsStream("encryptedmessage.gpg"));
+
+	    Map<String, Object> properties = new HashMap<String, Object>();
+	    properties.put("MULE_USER", "Test Testsson (This is just a test key please do not trust it.) <test.testsson@mailinator.com>");
+	    MuleMessage result = client.send("vm://Test2", payload, properties);
+	    Assert.assertEquals(expectedPayload, result.getPayloadAsString());
+	}
+
+	@Test
 	public void testThereAndBackAgain() throws Exception
 	{
 	    MuleClient client = new MuleClient(muleContext);
